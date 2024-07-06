26543c8d0ea4594685d09e73ed8216c13c4d054d
SakshamGrover09 <groversaksham2001@gmail.com>
2024-07-06 23:06:46 +0530

    backend notes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const { body , validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt=require('jsonwebtoken');
 const JWT_SECRET='changululla18';
+const fetchuser=require('../middleware/fetchuser');
 
 router.post('/createuser',[
     body('name','Enter valid name').isLength({min:3}),
@@ -55,6 +56,80 @@ router.post('/createuser',[
     }
 
     
+});
+
+router.post('/login',[
+  
+  body('email','Enter valid email').isEmail(),
+  body('password','Password cannot be empty').exists()
+
+],async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email,password}=req.body;
+  try {
+
+    let user= await User.findOne({email});
+    if(!user)
+    {
+      return res.status(400).json({error:"No User Found "});
+    }
+
+    const passwordCompare= await bcrypt.compare(password,user.password);
+    if(!passwordCompare)
+    {
+      return res.status(400).json({error:"No User Found "});
+    }
+
+    const data={
+
+      user:{
+        id:user.id
+      }
+    }
+
+
+    const authToken=jwt.sign(data,JWT_SECRET);
+    res.json({authToken});
+    
+
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+
+    
+  }
+
+  
+
+  
+});
+
+router.post('/getUser',fetchuser,async (req, res) => {
+  
+
+  
+  try {
+
+    const userId=req.user.id;
+    const user= await User.findById(userId).select("-password");
+    res.send(user);
+
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+
+    
+  }
+
+  
+
+  
 });
 
 module.exports=router
